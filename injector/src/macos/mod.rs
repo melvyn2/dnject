80164fc0da3a7bc49d014afa385da00b903f7fc8
melvyn <melvyn2@dnsense.pub>
2023-05-11 19:07:44 -0700

    Move remote arch checking fn to mach_util

--- injector/src/macos/mod.rs
+++ injector/src/macos/mod.rs
@@ -1,19 +1,16 @@
 use std::alloc::{Allocator, Global, Layout};
 use std::ffi::{c_char, c_int, c_ulong, c_void, CStr, CString};
 use std::io::ErrorKind;
-use std::mem::{size_of, MaybeUninit};
+use std::mem::size_of;
 use std::os::unix::ffi::OsStrExt;
 use std::path::PathBuf;
 use std::process::{Child, Command};
-use std::ptr::{addr_of, addr_of_mut, copy_nonoverlapping, null, null_mut};
+use std::ptr::{addr_of, addr_of_mut, copy_nonoverlapping, null};
 use std::sync::atomic::{fence, AtomicU8, Ordering};
 use std::thread::sleep;
 use std::time::Duration;
 
-use libc::{
-    c_uint, cpu_type_t, pid_t, sysctl, sysctlnametomib, CTL_KERN, KERN_PROC, KERN_PROC_PID,
-    _SC_PAGESIZE,
-};
+use libc::{pid_t, _SC_PAGESIZE};
 
 use mach2::boolean::boolean_t;
 use mach2::mach_port::mach_port_deallocate;
@@ -28,98 +25,12 @@ use mach2::vm_types::{mach_vm_address_t, mach_vm_size_t};
 
 use mach_util::ldsyms::{_mh_execute_header, getsectiondata};
 use mach_util::mach_error::mach_error_string;
-use mach_util::mach_try;
-use mach_util::machine::{CPU_TYPE_ANY, CPU_TYPE_ARM64, CPU_TYPE_X86_64};
-use mach_util::proc::P_TRANSLATED;
-use mach_util::sysctl::{kinfo_proc, CTL_MAXNAME};
 use mach_util::tasks::thread_create_running;
 use mach_util::thread_act::thread_terminate;
 use mach_util::traps::pid_for_task;
+use mach_util::{get_pid_cpu_type, mach_try};
 
 mod bootstrap;
-
-fn get_pid_cpu_type(pid: pid_t) -> Result<cpu_type_t, std::io::Error> {
-    // See https://github.com/objective-see/ProcessMonitor/blob/1a9c2e0c8044ad10676efad480f200f12060ed7a/Library/Source/Process.m#L252
-
-    let mut mib: [c_int; CTL_MAXNAME] = [0; CTL_MAXNAME];
-    let mut mib_length = CTL_MAXNAME;
-    let sysctl_str = CStr::from_bytes_with_nul(b"sysctl.proc_cputype\0")
-        .unwrap()
-        .as_ptr();
-    unsafe {
-        let res = sysctlnametomib(sysctl_str, mib.as_mut_ptr(), addr_of_mut!(mib_length));
-        if res != 0 {
-            return Err(std::io::Error::new(
-                std::io::Error::last_os_error().kind(),
-                "sysctlnametomib proc_cputype failed",
-            ));
-        };
-    }
-    // mib_length now contains the number of written elements, not total elements
-    // Write target pid to the next unwritten element
-    mib[mib_length] = pid;
-    mib_length += 1;
-
-    let mut cpu_type: cpu_type_t = CPU_TYPE_ANY;
-    let mut cpu_type_size = size_of::<cpu_type_t>();
-
-    unsafe {
-        let res = sysctl(
-            mib.as_mut_ptr(),
-            mib_length as c_uint,
-            addr_of_mut!(cpu_type) as *mut c_void,
-            addr_of_mut!(cpu_type_size),
-            null_mut(),
-            0,
-        );
-        if res != 0 {
-            return Err(std::io::Error::new(
-                std::io::Error::last_os_error().kind(),
-                "sysctl proc cputype failed",
-            ));
-        };
-    }
-    if cpu_type == CPU_TYPE_ANY {
-        return Err(std::io::Error::new(
-            ErrorKind::InvalidData,
-            "Invalid/missing CPU type returned",
-        ));
-    }
-
-    if cpu_type == CPU_TYPE_ARM64 {
-        let mut mib: [c_int; CTL_MAXNAME] = [0; CTL_MAXNAME];
-        mib[0] = CTL_KERN;
-        mib[1] = KERN_PROC;
-        mib[2] = KERN_PROC_PID;
-        mib[3] = pid;
-        let procinfo = unsafe {
-            let mut procinfo = MaybeUninit::<kinfo_proc>::zeroed();
-            let mut procinfo_size = size_of::<kinfo_proc>();
-            let res = sysctl(
-                mib.as_mut_ptr(),
-                4,
-                procinfo.as_mut_ptr() as *mut c_void,
-                addr_of_mut!(procinfo_size),
-                null_mut(),
-                0,
-            );
-            if res != 0 {
-                return Err(std::io::Error::new(
-                    std::io::Error::last_os_error().kind(),
-                    "sysctl procinfo failed",
-                ));
-            }
-            procinfo.assume_init()
-        };
-        if procinfo.kp_proc.p_flag & P_TRANSLATED != 0 {
-            Ok(CPU_TYPE_X86_64)
-        } else {
-            Ok(cpu_type)
-        }
-    } else {
-        Ok(cpu_type)
-    }
-}
 
 fn ensure_matching_arch(target: pid_t) -> Result<(), std::io::Error> {
     let target_arch = get_pid_cpu_type(target)?;
--- mach_util/src/lib.rs
+++ mach_util/src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(extern_types)]
 
-/// C-defs which aren't included in mach2, as well as helper classes
+//! C-defs which aren't included in mach2, as well as helper classes
 
 // Re-export for macro use
 #[doc(hidden)]
@@ -32,3 +32,94 @@ mod stubs;
 pub use stubs::*;
 
 pub mod mach_portal;
+
+pub fn get_pid_cpu_type(pid: libc::pid_t) -> Result<libc::cpu_type_t, std::io::Error> {
+    // See https://github.com/objective-see/ProcessMonitor/blob/1a9c2e0c8044ad10676efad480f200f12060ed7a/Library/Source/Process.m#L252
+
+    use crate::machine::{CPU_TYPE_ANY, CPU_TYPE_ARM64, CPU_TYPE_X86_64};
+    use crate::proc::P_TRANSLATED;
+    use crate::sysctl::{kinfo_proc, CTL_MAXNAME};
+    use libc::{c_int, c_uint, c_void, cpu_type_t, sysctl};
+    use std::ffi::CStr;
+    use std::mem::size_of;
+    use std::ptr::{addr_of_mut, null_mut};
+
+    let mut mib: [c_int; CTL_MAXNAME] = [0; CTL_MAXNAME];
+    let mut mib_length = CTL_MAXNAME;
+    let sysctl_str = CStr::from_bytes_with_nul(b"sysctl.proc_cputype\0")
+        .unwrap()
+        .as_ptr();
+    unsafe {
+        let res = libc::sysctlnametomib(sysctl_str, mib.as_mut_ptr(), addr_of_mut!(mib_length));
+        if res != 0 {
+            return Err(std::io::Error::new(
+                std::io::Error::last_os_error().kind(),
+                "sysctlnametomib proc_cputype failed",
+            ));
+        };
+    }
+    // mib_length now contains the number of written elements, not total elements
+    // Write target pid to the next unwritten element
+    mib[mib_length] = pid;
+    mib_length += 1;
+
+    let mut cpu_type: cpu_type_t = CPU_TYPE_ANY;
+    let mut cpu_type_size = size_of::<cpu_type_t>();
+
+    unsafe {
+        let res = sysctl(
+            mib.as_mut_ptr(),
+            mib_length as c_uint,
+            addr_of_mut!(cpu_type) as *mut c_void,
+            addr_of_mut!(cpu_type_size),
+            null_mut(),
+            0,
+        );
+        if res != 0 {
+            return Err(std::io::Error::new(
+                std::io::Error::last_os_error().kind(),
+                "sysctl proc cputype failed",
+            ));
+        };
+    }
+    if cpu_type == CPU_TYPE_ANY {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Invalid/missing CPU type returned",
+        ));
+    }
+
+    if cpu_type == CPU_TYPE_ARM64 {
+        let mut mib: [c_int; CTL_MAXNAME] = [0; CTL_MAXNAME];
+        mib[0] = libc::CTL_KERN;
+        mib[1] = libc::KERN_PROC;
+        mib[2] = libc::KERN_PROC_PID;
+        mib[3] = pid;
+        let procinfo = unsafe {
+            let mut procinfo = std::mem::MaybeUninit::<kinfo_proc>::zeroed();
+            let mut procinfo_size = size_of::<kinfo_proc>();
+            let res = sysctl(
+                mib.as_mut_ptr(),
+                4,
+                procinfo.as_mut_ptr() as *mut c_void,
+                addr_of_mut!(procinfo_size),
+                null_mut(),
+                0,
+            );
+            if res != 0 {
+                return Err(std::io::Error::new(
+                    std::io::Error::last_os_error().kind(),
+                    "sysctl procinfo failed",
+                ));
+            }
+            procinfo.assume_init()
+        };
+        if procinfo.kp_proc.p_flag & P_TRANSLATED != 0 {
+            Ok(CPU_TYPE_X86_64)
+        } else {
+            Ok(cpu_type)
+        }
+    } else {
+        Ok(cpu_type)
+    }
+}
